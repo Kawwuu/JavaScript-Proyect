35652b7864e58564d95de2e72c7b245bcb7b1191
Kawu <lucasrolla08@gmail.com>
2022-07-08 20:59:25 -0300

    Retoques en el funcionamiento del simulador interactivo

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-let precioProducto = parseFloat(prompt("Ingrese el precio del producto y obtendrá el cálculo sobre el iva (Puede incluir centavos)"));
-
-while (true) {
-
-    if (!isNaN(precioProducto) && precioProducto != null && precioProducto != ""){
-        break;
-    } else{
-        alert('El valor ingresado no es correcto');
-        continue;
-    }
-}
-precioProductoP = precioProducto.toFixed(2);
-alert("El precio del producto a calcular es: " + precioProductoP);
-console.log("El precio del producto a calcular es: " + precioProductoP + " ");
-
-
-
+let precioProducto = Number(prompt("Ingrese el precio del producto y obtendrá el cálculo sobre el iva (Puede incluir centavos)"));
 
 function precioIva(precioProducto){
-    return (precioProducto * 0.21)
+    if(!isNaN(precioProducto)){
+        let resultado = precioProducto * 1.21
+        alert("El precio del producto es: " + resultado)
+    } else {
+        alert("Debes ingresar un numero")
+    }
 }
 
-let iva = precioIva(precioProducto);
-ivaI = iva.toFixed(2);
+let llamarResultado = precioIva(precioProducto)
 
-alert("El impuesto (iva) calculado sobre su producto es: " + ivaI);
-console.log("El impuesto (iva) calculado sobre su producto es: " + ivaI + " ");
-
-
-
-function precioProductoConIva(precioProducto){
-    return(precioProducto + (precioProducto * 0.21));
-}
-
-let precioFinal = precioProductoConIva(precioProducto);
-precioFinalF = precioFinal.toFixed(2);
-
-
-alert("El precio final del producto con impuestos incluidos es: " + precioFinalF);
-console.log("El precio final del producto con impuestos incluidos es: " + precioFinalF);
