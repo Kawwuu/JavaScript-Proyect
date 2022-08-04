03efed9efb1e7e1cdd1c1e00854ab800dffa6f0e
Lucas Rolla <lucasrolla08@gmail.com>
2022-08-03 22:13:43 -0300

    Agregando sweetalert2

--- js/app.js
+++ js/app.js
@@ -2,14 +2,87 @@ let littleLetters;
 let numberErrors = 0;
 let numberHits = 0; 
 
-const lettersArray = JSON.stringify(letters);
-localStorage.setItem("letters", lettersArray);
-const arrayLetters = localStorage.getItem("letters");
-
 const btn = id('play');
 const image = id('image');
 const btn_letters = document.querySelectorAll('#letters button');
+const buttonUser = document.querySelector("#button-user");
 
+async function userDate(){
+    const {
+    value: formData
+} =  await Swal.fire({
+        title: 'Colocar nombre de juego',
+        html: '<div class="form-inline col-sm-12 mt-3">' +
+        '<label class="control-label col-sm-4" for="UserName">Nombre</label>' +
+        '<input required="" class="form-control col-sm-7" id="UserName" name="usuario" type="text" autofocus style="color: #2e7d32;">' +
+        '</div>',
+        backdrop: false,
+        showConfirmButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Validar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#40B340',
+        cancelButtonColor: '#FF0000',
+        showCloseButton: true,
+        focusConfirm: true,
+        focusCancel: false,
+        preConfirm: () => {
+        return [
+            document.getElementById('UserName').value
+        ]
+        },
+        onOpen: (modal) => {
+            confirmOnEnter = (event) => {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                modal.querySelector(".swal2-confirm").click();
+            }
+            };
+            modal.querySelector("#UserName").addEventListener("keyup", confirmOnEnter);
+        }
+        });
+        if (formAutenticar && formAutenticar[0] != '' && formAutenticar[1] != '') {
+        Swal.fire({
+            title: 'Dato Correcto',
+            icon: 'success',
+            backdrop: false
+        });
+    } else {
+        Swal.fire({
+            title: 'Dato incorrecto',
+            icon: 'warning',
+            backdrop: false
+        });
+    }}
+
+const dateX = JSON.stringify(userDate);
+localStorage.setItem("date", userDate);
+const xDate = localStorage.getItem("date");
+
+
+btn.addEventListener ("click", () => {
+    let timerInterval
+Swal.fire({
+    title: 'Generando palabra',
+    html: 'Generando palabra en <b></b> milisegundos.',
+    timer: 1000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+        }, 100)
+    },
+    willClose: () => {
+        clearInterval(timerInterval)
+    }
+    }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
+    }
+})
+})
 
 function id(str) {
     return document.getElementById(str);
